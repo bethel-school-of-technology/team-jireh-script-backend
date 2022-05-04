c6f5d08f012826647fbcde5628f95524c6fe7836
bhall21 <bhall228@gmail.com>
2022-05-04 15:03:34 -0700

    incomplete route to upload images

--- Econex/app.js
+++ Econex/app.js
@@ -10,6 +10,7 @@ var session = require('express-session');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const { sequelize } = require('./models');
+var imageUploadRouter = require('./routes/imageUpload');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/imageUpload', imageUploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
