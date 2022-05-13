950efe5e615510d0564d4a4eff4744c991a775ca
Micah Merkel <micahmerkel@icloud.com>
2022-05-13 13:36:36 -0500

    Malfunctioning credit route

--- Econex/models/users.js
+++ Econex/models/users.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
      this.hasMany(models.categories, models.creditcards,{
-       foriegnKey: 'UserId'
+       foriegnKey: 'Id'
      })}
   };
   Users.init({
